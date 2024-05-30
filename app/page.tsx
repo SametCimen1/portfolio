9d0bd3a828f98d2471483d4836ea393fb0adeae3
Samet Cimen <cimensamet338@gmail.com>
2024-05-30 16:26:37 -0400

    template done

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,12 @@ import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
 
+import { GlobeDemo } from "@/components/ui/GridGlobe";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden  mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className = "max-w-7xl w-full">
         <FloatingNav navItems = {navItems}
         />  
@@ -21,6 +24,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
