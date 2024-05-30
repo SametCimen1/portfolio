8f23e4110093381462d794a3e578cd09242b4113
Samet Cimen <cimensamet338@gmail.com>
2024-05-30 10:30:55 -0400

    Added projects & client section

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden  mx-auto sm:px-10 px-5">
       <div className = "max-w-7xl w-full">
-        <FloatingNav 
-          navItems={[{
-            name: "Home",
-            link: '/',
-            icon: <FaHome/>
-          }]}
+        <FloatingNav navItems = {navItems}
         />  
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
+        <Experience />
       </div>
     </main>
   );
